90949d4bfae503c6bff8f955d99b40562cb70ea5
mhaya <masaharu.hayashi3@gmail.com>
2019-08-20 22:37:24 +0900

    update

--- invenio_simplemodule/views.py
+++ invenio_simplemodule/views.py
@@ -16,6 +16,7 @@ from __future__ import absolute_import, print_function
 from flask import Blueprint, render_template
 from flask_babelex import gettext as _
 from invenio_app.factory import instance_path, static_folder
+from flask import current_app as flask_current_app
 import os
 
 blueprint = Blueprint(
